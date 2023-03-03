3266267ad8a7d7779cecb791613d5740084c36ce
Akash Chandra Debnath <66691792+Akash-Debnath@users.noreply.github.com>
2023-03-03 18:13:40 +0600

    Combined the two for loops, use Arrays.stream()

--- ChemistryCalculator/backend/EquationBalancer.java
+++ ChemistryCalculator/backend/EquationBalancer.java
@@ -1,5 +1,7 @@
 package ChemistryCalculator.backend;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class EquationBalancer {
     private final String reactants;
@@ -11,10 +13,8 @@ public class EquationBalancer {
     }
 
     public String balance() {
-        String balancedEquation;
-        //removing all whitespace
-        String[] reactants = this.reactants.replaceAll("\\s+", "").split("\\+");
-        String[] products = this.products.replaceAll("\\s+", "").split("\\+");
+        String[] reactants = Arrays.stream(this.reactants.replaceAll("\\s+", "").split("\\+")).map(String::trim).toArray(String[]::new);
+        String[] products = Arrays.stream(this.products.replaceAll("\\s+", "").split("\\+")).map(String::trim).toArray(String[]::new);
 
         CompoundManager manager = new CompoundManager(reactants[0]);
         for (int i = 1; i < reactants.length; i++) {
@@ -26,59 +26,36 @@ public class EquationBalancer {
             manager.append(products[i], i + reactants.length, -1);
         }
 
-
-        //mapping  ArrayList<ArrayList<Integer>> to int[][] as Matrix class takes int[][].
-        int[][] elementsMatrix = manager.getElementMatrix().stream().map(u -> u.stream().mapToInt(i -> i).toArray()).toArray(int[][]::new);
-
+        int[][] elementsMatrix = manager.getElementMatrix().stream().map(list -> list.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new);
         Matrix matrix = new Matrix(elementsMatrix);
         Fraction[] nullSpace = matrix.transpose().nullSpace();
-        if (nullSpace != null) {
-            long[] balancedCoefficient = Fraction.normalize(nullSpace);
 
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < reactants.length; i++) {
-                if (i == reactants.length - 1) {
-                    if (balancedCoefficient[i] == 1) {
-                        result.append(reactants[i]).append(" ");
-                    } else {
-                        result.append(balancedCoefficient[i]).append(reactants[i]).append(" ");
-                    }
-                } else {
-                    if (balancedCoefficient[i] == 1) {
-                        result.append(reactants[i]).append(" + ");
-                    } else {
-                        result.append(balancedCoefficient[i]).append(reactants[i]).append(" + ");
-                    }
-                }
-
-            }
-
-            result.append("= ");
-
-
-            for (int i = 0; i < products.length; i++) {
-                if (i == products.length - 1) {
-                    if (balancedCoefficient[i + reactants.length] == 1) {
-                        result.append(products[i]).append(" ");
-                    } else {
-                        result.append(balancedCoefficient[i + reactants.length]).append(products[i]).append(" ");
-                    }
-                } else {
-                    if (balancedCoefficient[i + reactants.length] == 1) {
-                        result.append(products[i]).append(" + ");
-                    } else {
-                        result.append(balancedCoefficient[i + reactants.length]).append(products[i]).append(" + ");
-                    }
-                }
-
-            }
-            balancedEquation = result.toString();
-
-
-        } else {
-            throw new InvalidEquationException("Error ! given equation is not vaild");
+        if (nullSpace == null) {
+            throw new InvalidEquationException("Error ! given equation is not valid");
         }
 
-        return balancedEquation;
+        long[] balancedCoefficients = Fraction.normalize(nullSpace);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < reactants.length; i++) {
+            if (i > 0) {
+                result.append(" + ");
+            }
+            result.append(balancedCoefficients[i]);
+            result.append(reactants[i]);
+        }
+
+        result.append(" = ");
+
+        for (int i = 0; i < products.length; i++) {
+            if (i > 0) {
+                result.append(" + ");
+            }
+            result.append(balancedCoefficients[i + reactants.length]);
+            result.append(products[i]);
+        }
+
+        return result.toString();
     }
 }
+
