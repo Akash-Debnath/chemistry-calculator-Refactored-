695ce466d232f4f4c3a8d3ea787611be5517fc74
Akash Chandra Debnath <66691792+Akash-Debnath@users.noreply.github.com>
2023-03-03 18:03:43 +0600

    Fixed duplicated code,primitive Obsession,Long method

--- ChemistryCalculator/backend/Atom.java
+++ ChemistryCalculator/backend/Atom.java
@@ -6,7 +6,7 @@ public class Atom {
     private static final HashMap<String, String[]> allAtoms = new Database().getAllAtoms();
     private String symbol;
     private String name;
-    private  double atomicMass;
+    private double atomicMass;
     private int atomicNumber;
 
     private Atom(String symbol) {
@@ -18,67 +18,68 @@ public class Atom {
 
     }
 
+
+
+    // Refactored Code by Akash Chandra Debnath
+
     private Atom(int atomicNumber) {
-        allAtoms.entrySet().stream().filter(entry -> atomicNumber == Integer.parseInt(entry.getValue()[0])).forEachOrdered(entry -> {
-            this.symbol = entry.getKey();
-            this.name = entry.getValue()[1];
-            this.atomicMass = Double.parseDouble(entry.getValue()[2]);
-        });
+        allAtoms.entrySet().stream()
+                .filter(entry -> atomicNumber == Integer.parseInt(entry.getValue()[0]))
+                .forEach(entry -> {
+                    this.symbol = entry.getKey();
+                    this.name = entry.getValue()[1];
+                    this.atomicMass = Double.parseDouble(entry.getValue()[2]);
+                });
         this.atomicNumber = atomicNumber;
     }
 
-    public static Atom getInstance(String symbol) {
-        if (isValid(symbol)) {
-            return new Atom(symbol);
-        }
-        throw new InvalidAtomException("'" + symbol + "'" + " is not a valid symbol !");
-    }
+   
+   
+   
+    // Refactored Code by Akash Chandra Debnath
 
-    public static Atom getInstance(int atomicNumber) {
-        if (atomicNumber >= 1 && atomicNumber <= 118){
+    private createAtom(String symbol, int atomicNumber) {
+        if (symbol != null && isValid(symbol)) {
+            return new Atom(symbol);
+        } else if (atomicNumber >= 1 && atomicNumber <= 118) {
             return new Atom(atomicNumber);
         }
-        throw new InvalidAtomException("'" + atomicNumber + "'" + " is not a valid atomic number !");
+        throw new InvalidAtomException("Invalid symbol or atomic number");
     }
+    
+    public static Atom getInstance(String symbol) {
+        return createAtom(symbol, 0);
+    }
+    
+    public static Atom getInstance(int atomicNumber) {
+        return createAtom(null, atomicNumber);
+    }
+
+
 
     private static boolean isValid(String symbol) {
         return allAtoms.containsKey(symbol);
     }
 
 
+
+    // Refactored Code by Akash Chandra Debnath
+
     public String getElectronConfig() {
-
-        int counter;
-        int tempAtomicNumber = this.atomicNumber;
-
+        int[] orbitalSizes = {2, 2, 6, 2, 6, 2, 10, 6, 2, 10, 6, 2, 14, 10, 6, 2, 14, 10, 6, 2};
         StringBuilder output = new StringBuilder();
-        String[][] orbitals = {
-                {"1s", "2"}, {"2s", "2"}, {"2p", "6"},
-                {"3s", "2"}, {"3p", "6"}, {"4s", "2"},
-                {"3d", "10"}, {"4p", "6"}, {"5s", "2"},
-                {"4d", "10"}, {"5p", "6"}, {"6s", "2"},
-                {"4f", "14"}, {"5d", "10"}, {"6p", "6"},
-                {"7s", "2"}, {"5f", "14"}, {"6d", "10"},
-                {"7p", "6"}, {"8s", "2"}
-        };
-
-        for (String[] innerArray : orbitals) {
-            String orbitalName = innerArray[0];
-            int orbitalSize = Integer.parseInt(innerArray[1]);
-
-            counter = Math.min(orbitalSize, tempAtomicNumber);
-
-            output.append(orbitalName).append(counter).append(" ");
-
-            tempAtomicNumber -= counter;
-            if (tempAtomicNumber <= 0) {
+        int remainingElectrons = atomicNumber;
+        for (int i = 0; i < orbitalSizes.length; i++) {
+            int electronsInOrbital = Math.min(orbitalSizes[i], remainingElectrons);
+            output.append((i + 1)).append("s").append(electronsInOrbital).append(" ");
+            remainingElectrons -= electronsInOrbital;
+            if (remainingElectrons == 0) {
                 break;
             }
-
         }
-
         return output.toString();
     }
+
 
     public String getSymbol() {
         return symbol;
